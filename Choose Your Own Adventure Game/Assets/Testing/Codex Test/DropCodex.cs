de3fd77fd6f554e1ca94ff211368efe0e434902d
Rhys Angelo-Jones <rhystaj@gmail.com>
2017-12-16 11:03:12 +1300

    Set up Codex Test
    Created a new testing folder and in it created a new text scene and test script deriving from codex.

---
+++ Choose Your Own Adventure Game/Assets/Testing/Codex Test/DropCodex.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * A codex with a simple drop down menu to choose the category.
+ */
+public class DropCodex : Codex
+{
+    public override void Display()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Hide()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override void CloseDirectory(CodexDirectory directory)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override void CloseEntry(CodexEntry entry)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override void OpenDirectory(CodexDirectory directory, CodexNode[] subnodes)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override void OpenEntry(CodexEntry entry, string text)
+    {
+        throw new NotImplementedException();
+    }
+}
