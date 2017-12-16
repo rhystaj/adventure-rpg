1ad520348210100b6f588bff6fa355a08002b1e0
Rhys Angelo-Jones <rhystaj@gmail.com>
2017-12-16 17:00:56 +1300

    Continue to implement DropCodex so that it shows the option of the currently selected category
    Still has issues at this point.

--- Choose Your Own Adventure Game/Assets/Testing/Codex Test/DropCodex.cs
+++ Choose Your Own Adventure Game/Assets/Testing/Codex Test/DropCodex.cs
@@ -10,8 +10,10 @@ using UnityEngine.UI;
 public class DropCodex : Codex
 {
     [SerializeField] Dropdown categoryDropdown; //The dropdown menu that will show the options for categories.
+    [SerializeField] DynamicButtonGroup optionsGroup; //The button group where the entry options will be displayed.
 
     private List<CodexNode> categories; //The list (in the order they appear) on entries in the dropdown.
+    private int currentOption = 0;
 
     private void Start()
     {
@@ -20,26 +22,47 @@ public class DropCodex : Codex
 
         //Add names to the dropdown.
         categoryDropdown.AddOptions(categoryNames);
+
+        categories[0].Retrieve(this);
+
     }
 
-    protected override void CloseDirectory(CodexDirectory directory)
+    public override void CloseDirectory(CodexDirectory directory)
+    {
+        optionsGroup.Clear();
+    }
+
+    public override void CloseEntry(CodexEntry entry)
     {
         throw new NotImplementedException();
     }
 
-    protected override void CloseEntry(CodexEntry entry)
+    public override void OpenDirectory(CodexDirectory directory)
+    {
+        int count = 0;
+        foreach(CodexNode node in directory.GetChildren())
+        {
+            optionsGroup.AddOption(count++, node.nodeName, () => { });
+        }
+
+        optionsGroup.DrawGroup();
+
+    }
+
+    public override void OpenEntry(CodexEntry entry, string text)
     {
         throw new NotImplementedException();
     }
 
-    protected override void OpenDirectory(CodexDirectory directory)
+    /**
+     * The response to when an option from the drop-down list is selected.
+     */ 
+    public void OnOptionNumberIsSelected(int option)
     {
+        categories[currentOption].Return(this);
+        categories[option].Retrieve(this);
 
-    }
-
-    protected override void OpenEntry(CodexEntry entry, string text)
-    {
-        throw new NotImplementedException();
+        currentOption = option;
     }
 
     //Redundant in this implementation.
