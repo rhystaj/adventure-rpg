7c582ae234633ac5e6517d25acf4f30f806ccaf8
Rhys Angelo-Jones <rhystaj@gmail.com>
2017-12-24 09:26:46 +1300

    Create CombatScenario script.

---
+++ Choose Your Own Adventure Game/Assets/Scripts/Combat/CombatScenario.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * The settings for a particular combat scenario.
+ */ 
+public class CombatScenario : MonoBehaviour {
+
+	
+
+}
