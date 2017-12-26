825f37095c4f5082384a57f9ed77b2a9a8e9e737
Rhys Angelo-Jones <rhystaj@gmail.com>
2017-12-26 13:46:56 +1300

    Create custom inspector for CombatScenario

--- Choose Your Own Adventure Game/Assets/Scripts/Combat/CombatScenario.cs
+++ Choose Your Own Adventure Game/Assets/Scripts/Combat/CombatScenario.cs
@@ -1,12 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 /**
  * The settings for a particular combat scenario.
  */ 
+[CreateAssetMenu]
 public class CombatScenario : ScriptableObject {
 
-	
+    [Header("Dimensions")]
+    public int columnsPerSide; //The number of columns on each of the TWO sides of the board, i.e. the actual number of colums will be double.
+    public int rows; //The number of rows the board has.
+
+    [HideInInspector]
+    public List<Unit> enemyConfiguration = new List<Unit>(); //The positioning of the units in the enemy team.
 
 }
---
+++ Choose Your Own Adventure Game/Assets/Scripts/Editor/CombatScenarioEditor.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+/**
+ * The custom inspector for CombatScenario
+ */ 
+[CustomEditor(typeof(CombatScenario))]
+public class CombatScenarioEditor : Editor {
+
+    const int UNIT_FIELD_WIDTH = 100;
+
+    private CombatScenario targetScenario; //The CombatScenario being modified by the editor.
+
+    private void OnEnable()
+    {
+        targetScenario = (CombatScenario)target;
+    }
+
+    override public void OnInspectorGUI()
+    {
+
+        base.OnInspectorGUI();
+
+        //Ensure the dimension can't have negative values.
+        if (targetScenario.rows < 0) targetScenario.rows = 0;
+        if (targetScenario.columnsPerSide < 0) targetScenario.columnsPerSide = 0;
+
+        //Adjust the length of targetScenario.enemyConfiguration if the number of rows and columns have changed.
+        while (targetScenario.enemyConfiguration.Count < targetScenario.rows * targetScenario.columnsPerSide)
+            targetScenario.enemyConfiguration.Add(null);
+        while (targetScenario.enemyConfiguration.Count > targetScenario.rows * targetScenario.columnsPerSide)
+            targetScenario.enemyConfiguration.Remove(null);
+
+        
+        GUILayout.Space(10);
+        DrawEnemyUnitPositionSelection();
+
+    }
+
+    /**
+     * Configure the part of the editor that would allow you to set the position of the enemy units.
+     */
+    private void DrawEnemyUnitPositionSelection()
+    {
+
+        EditorGUILayout.LabelField("Enemy Positions", EditorStyles.boldLabel);
+
+        int i = 0;
+        for(int row = 0; row < targetScenario.rows; row++)
+        {
+
+            GUILayout.BeginHorizontal();
+
+            for (int start = row * targetScenario.columnsPerSide; i - start < targetScenario.columnsPerSide; i++)
+                targetScenario.enemyConfiguration[i] = (Unit)EditorGUILayout.ObjectField(targetScenario.enemyConfiguration[i],
+                                                                                   typeof(Unit), true, GUILayout.Width(UNIT_FIELD_WIDTH));
+
+            GUILayout.EndHorizontal();
+
+        }
+
+    }
+
+}
