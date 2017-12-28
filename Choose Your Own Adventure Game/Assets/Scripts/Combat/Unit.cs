7ee2355fad76489d76c505569426b27d6e516ec8
Rhys Angelo-Jones <rhystaj@gmail.com>
2017-12-29 10:37:02 +1300

    Make Unit a MonoBehaviour and add a position field.

--- Choose Your Own Adventure Game/Assets/Scripts/Combat/Unit.cs
+++ Choose Your Own Adventure Game/Assets/Scripts/Combat/Unit.cs
@@ -6,24 +6,33 @@ using UnityEngine;
  * A piece that can be placed on the board during combat.
  */ 
 [CreateAssetMenu]
-public class Unit : ScriptableObject {
+public class Unit : MonoBehaviour {
 
     [Header("Stats")]
-    [SerializeField] Instrument instument; //The main item the unit uses when moving.
-    [SerializeField] float maxHealth; //The max health points of the unit.
-    [SerializeField] float effectiveness; //The unit's ability with thier instrument.
-    [SerializeField] int alignment; //The team this unit belongs to.
+    public Instrument instument; //The main item the unit uses when moving.
+    public float maxHealth; //The max health points of the unit.
+    public float effectiveness; //The unit's ability with thier instrument.
+    public int alignment; //The team this unit belongs to.
     [Space(10)]
 
     [Header("Graphics")]
-    [SerializeField] Sprite neutralSprite;
-    [SerializeField] Sprite attakingSprite;
+    public Sprite neutralSprite;
+    public Sprite attakingSprite;
     [SerializeField] Sprite talkingDamageSprite;
+
+    [HideInInspector] public int position;
+
+    private SpriteRenderer spriteRenderer;
 
     public int Alignment { get { return alignment; } }
 
     private void OnEnable()
     {
+
+        //Get the respective game object's sprite render, or throw an error if it doesn't have one.
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if (spriteRenderer == null) Debug.LogError("The unit " + name + " does not have a renderer attatched.");
+        spriteRenderer.sprite = neutralSprite;
 
         //Ensure stats aren't negative.
         if (maxHealth < 0) maxHealth = 0;
