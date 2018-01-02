5b640c36042bea284a83ad65fded24e08c198582
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-01-02 16:23:26 +1300

    Create CombatFlow abstact class.

---
+++ Choose Your Own Adventure Game/Assets/Scripts/Combat/Structure/CombatFlow.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * Specifies the turn progression of a combat scenario.
+ */ 
+public abstract class CombatFlow {
+
+    protected Turn currentTurn; //The next turn to be taken.
+
+    public int CurrentTeam { get { return currentTurn.Team; } } //Returns the team taking the current turn.
+
+    //Returns the units avaliable for the current turn.
+    protected HashSet<Unit> UnitsAvaliableForTurn { get { return new HashSet<Unit>(currentTurn.AvaliableUnits); } }
+
+    public void TakeTurn()
+    {
+        currentTurn = currentTurn.take();
+    }
+
+    protected abstract class Turn
+    {
+        private int team;
+        private HashSet<Unit> avaliableUnits;
+
+        public int Team { get { return team; } }
+        public HashSet<Unit> AvaliableUnits { get { return avaliableUnits; } }
+
+        public Turn(int team, HashSet<Unit> avaliableUnits)
+        {
+            this.team = team;
+            this.avaliableUnits = avaliableUnits;
+        }
+
+        /**
+         * Have the unit take the turn, if they are allowed to.
+         */ 
+        public abstract Turn take(Unit subject);
+
+        public bool CanMove(Unit unit)
+        {
+            return avaliableUnits 
+        }
+
+    }
+
+}
