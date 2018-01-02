e28a877384fa86377e0f3225e400cbace5063b23
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-01-03 11:32:31 +1300

    Add assertions to CodexDirectory.

--- Choose Your Own Adventure Game/Assets/Scripts/Codex/CodexDirectory.cs
+++ Choose Your Own Adventure Game/Assets/Scripts/Codex/CodexDirectory.cs
@@ -1,6 +1,7 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 /**
      * A branching node or category.
@@ -11,13 +12,27 @@ public class CodexDirectory : CodexNode
 
     [SerializeField] CodexNode[] contents; //The items in the current directory.
 
+    //Assetion fields.
+    private CodexNode[] contentsOnEnable; //Represents the configuration of the contents array on enable.
+
     private void OnEnable()
     {
+        //Assertion-only setup methods.
+        Assert.IsTrue(RecordContents());
+
+        //Preconditions
+        Assert.IsNotNull(contents, "Contents should not be null.");
+
         //Make this the parent of each of the given nodes.
         foreach (CodexNode node in contents)
             node.SetParent(this);
 
         retrieved = false;
+
+        //Postconditions
+        Assert.IsTrue(ClassInvariantsHold());
+        Assert.IsFalse(retrieved, "Retrieved should initiall be false.");
+        Assert.IsTrue(ThisParentOfAllContentNodes());
     }
 
     /**
@@ -25,43 +40,101 @@ public class CodexDirectory : CodexNode
      */
     public List<CodexNode> GetChildren()
     {
-        return new List<CodexNode>(contents);
+        List<CodexNode> result = new List<CodexNode>(contents);
+
+        //Postconditions
+        Assert.IsTrue(ClassInvariantsHold());
+
+        return result;
     }
 
     /**
      * Returns wheter a given node is a DIRECT child of the directory node.
      */
-    public bool NodeInDirectory(CodexNode node)
-    {
+    public bool NodeInDirectory(CodexNode node) { 
+
+        bool result = false;
+
         foreach (CodexNode n in contents)
         {
-            if (n.Equals(node)) return true;
+            if (n.Equals(node)){
+                result = true;
+                break;
+            }
         }
 
-        return false;
+        //Postconditions
+        Assert.IsTrue(ClassInvariantsHold());
+
+        return result;
+
     }
 
     public override void Retrieve(Codex codex)
     {
 
-        Debug.Log(retrieved);
+        //Preconditions
+        Assert.IsNotNull(codex, "The given codex should not be null");
 
-        if (retrieved) return;
-        codex.OpenDirectory(this);
+        if (!retrieved)
+        {
+            codex.OpenDirectory(this);
+            retrieved = true;
+        }
 
-        retrieved = true;
+        //Postconditions
+        Assert.IsTrue(ClassInvariantsHold());
+        Assert.IsTrue(true, "The field 'retrieved' should be true after this method has been called.");
 
     }
 
     public override void Return(Codex codex)
     {
 
-        if (!retrieved) return;
+        //Preconditions
+        Assert.IsNotNull(codex, "The given codex should not be null");
 
-        foreach (CodexNode node in contents) node.Return(codex); //Recursively return all children.
-        codex.CloseDirectory(this);
+        if (retrieved)
+        {
+            foreach (CodexNode node in contents) node.Return(codex); //Recursively return all children.
+            codex.CloseDirectory(this);
 
-        retrieved = false;
+            retrieved = false;
+        }
+
+        //Postconditions
+        Assert.IsTrue(ClassInvariantsHold());
+        Assert.IsTrue(AllChildernNotRetrieved());
+        Assert.IsFalse(retrieved, "The field 'retrieved' should be false after this method has been called.");
+
+    }
+
+    //Assertion Methods
+    private bool RecordContents()
+    {
+        Array.Copy(contents, contentsOnEnable, contents.Length);
+        return true;
+    }
+
+    private bool ClassInvariantsHold()
+    {
+        Assert.AreEqual(contents, contentsOnEnable, "The list of entries should never be changed at runtime.");
+
+        return true;
+    }
+
+    private bool ThisParentOfAllContentNodes()
+    {
+        foreach (CodexNode node in contents)
+            Assert.IsTrue(node.HasParent(this), name + "should be the parent of " + node.name);
+        return true;
+    }
+
+    private bool AllChildernNotRetrieved()
+    {
+        foreach (CodexNode node in contents)
+            Assert.IsFalse(node.Retrieved, node.name + "should not be retrieved.");
+        return true;
     }
 
 }
