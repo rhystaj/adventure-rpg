bb5b1fdff2e8e36d2215e6c7eeb3e541b1ebf0f2
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-01-20 12:40:53 +1300

    Add a test ensuring a unit can move if it is in the avaliable units for a turn tp CombatFlowTests.

--- Choose Your Own Adventure Game/Assets/Scripts/Unit Testing/CombatFlowTests.cs
+++ Choose Your Own Adventure Game/Assets/Scripts/Unit Testing/CombatFlowTests.cs
@@ -2,6 +2,8 @@
 using UnityEngine.TestTools;
 using NUnit.Framework;
 using System.Collections;
+using NSubstitute;
+using System.Collections.Generic;
 
 public class CombatFlowTests {
 
@@ -12,7 +14,15 @@ public class CombatFlowTests {
     public void AUnitInAvaliableUnitsCanMove()
     {
 
-        
+        Unit targetUnit = new Unit(Substitute.For<Instrument>(), 23, 4, 1);
+        HashSet<Unit> avalaibleUnits = new HashSet<Unit>(new Unit[] {
+            targetUnit,
+            new Unit(Substitute.For<Instrument>(), 6, 7, 8),
+            new Unit(Substitute.For<Instrument>(), 23, 5, 1)
+        });
+        CombatFlow testFlow = new SingleEmptyTurnMockCombatFlow(1, avalaibleUnits);
+
+        Assert.IsTrue(testFlow.CanMoveDuringCurrentTurn(targetUnit));
 
     }
 
