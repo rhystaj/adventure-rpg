4fc8f577bbe123733cd60306b365e83659a7d4bf
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-01-20 11:48:33 +1300

    Create CombatFlowTests test class.

---
+++ Choose Your Own Adventure Game/Assets/Scripts/Unit Testing/CombatFlowTests.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class CombatFlowTests {
+
+    /**
+     * Ensures that a unit can move in a turn in which it is listed in avaliable units.
+     */ 
+	[Test]
+    public void AUnitInAvaliableUnitsCanMove()
+    {
+
+        
+
+    }
+
+}
