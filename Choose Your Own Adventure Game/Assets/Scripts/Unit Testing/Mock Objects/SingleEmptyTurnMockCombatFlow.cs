ade4dda93a3594b6e39db13aac90a50aeefb63f5
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-01-20 12:39:53 +1300

    Create SimpleEmptyTurnMockCombatFlow mock object for testing.

---
+++ Choose Your Own Adventure Game/Assets/Scripts/Unit Testing/Mock Objects/SingleEmptyTurnMockCombatFlow.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * A mock combat flow with a single turn that does nothing when taken.
+ */ 
+public class SingleEmptyTurnMockCombatFlow : CombatFlow {
+
+	public SingleEmptyTurnMockCombatFlow(int team, HashSet<Unit> avaliableUnits)
+    {
+        currentTurn = new EmptyTurn(team, avaliableUnits);
+    }
+
+    private class EmptyTurn : Turn
+    {
+
+        public EmptyTurn(int team, HashSet<Unit> avaliableUnits) : base(team, avaliableUnits){ /* No addition functionality needed */ }
+
+        public override Turn take(Unit subject) { return this; /* Is a mock, so does nothing. */ }
+
+    }
+
+
+}
