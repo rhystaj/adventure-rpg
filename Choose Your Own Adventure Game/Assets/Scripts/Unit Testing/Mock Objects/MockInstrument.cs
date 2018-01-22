f3223458ee63ed590fa3d48f7e27a10db9f77952
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-01-23 09:59:55 +1300

    Create a MockInstrument class for testing.

---
+++ Choose Your Own Adventure Game/Assets/Scripts/Unit Testing/Mock Objects/MockInstrument.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MockInstrument : Instrument
+{
+    public override bool Use(Unit user, Unit target){ return false; }
+}
