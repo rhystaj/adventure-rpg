d26bd8615856e98903e951b6d8c787a4c71e5a87
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-01-23 11:19:36 +1300

    Create a UnitTest class and add a test than ennsures the Unit's health is set to the max on enable.

---
+++ Choose Your Own Adventure Game/Assets/Scripts/Unit Testing/UnitTests.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class UnitTests : MonoBehaviour {
+
+    /**
+     * Ensures that the helth of a unit is set to its max health when it is first created.
+     */ 
+	[UnityTest]
+    public IEnumerator UnitsHealthIsSetToMaxOnEnable()
+    {
+
+        GameObject basePrefab = Resources.Load<GameObject>("Testing/Mock Prefabs/Combat/Units/Flow Test Units/Flow Test Mock Unit 1");
+        Unit testUnit = Instantiate(basePrefab).GetComponent<Unit>();
+
+        yield return null; //Wait a frame, when everting is set up.
+
+        Assert.AreEqual(testUnit.health, testUnit.maxHealth, 
+                        "testUnit.health (" + testUnit.health + ") should be equal to testUnit.maxHealth (" + testUnit.maxHealth + ").");
+
+    }
+
+}
