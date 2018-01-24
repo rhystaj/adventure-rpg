8820b84d9fed9b3bbaa696699c319262884e9183
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-01-25 08:58:46 +1300

    Create a test class for testing TestingUtil.

---
+++ Choose Your Own Adventure Game/Assets/Scripts/Unit Testing/TestingUtilTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingUtilTests {
+
+    [Test]
+    public void CountItemsForWhichHoldsReturnsCorrectNumber()
+    {
+
+        string[] strings = new string[]
+        {
+            "rhys",
+            "ryan",
+            "brett",
+            "saylor",
+            "rachel"
+        };
+
+        Assert.IsTrue(TestingUtil.CountItemsForWhichHolds(strings, str => str.StartsWith("r")) == 3);
+
+    }
+
+}
