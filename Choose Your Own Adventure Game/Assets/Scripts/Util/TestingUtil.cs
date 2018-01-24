54dc6488ff86dea1acf2d867f57aeca78d0383de
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-01-25 08:50:48 +1300

    Create a utility class with methods for help with testing.

---
+++ Choose Your Own Adventure Game/Assets/Scripts/Util/TestingUtil.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * Contains static helper methods for testing.
+ */ 
+public class TestingUtil {
+
+	public static string PrintsItemsOf<T>(IEnumerable<T> set)
+    {
+
+        string returnString = "\n";
+
+        foreach(T item in set)
+        {
+            returnString = returnString + "- " + item + "\n";
+        }
+
+        return returnString;
+
+    }
+
+    /**
+     * Counts the number of items produced by the given Emumerable for which the given condition holds.
+     */ 
+    public static int CountItemsForWhichHolds<T>(IEnumerable<T> collection, Predicate<T> condition)
+    {
+
+        int count = 0;
+
+        foreach (T item in collection)
+            if (condition.Invoke(item)) count++;
+
+        return count;
+
+    }
+
+}
