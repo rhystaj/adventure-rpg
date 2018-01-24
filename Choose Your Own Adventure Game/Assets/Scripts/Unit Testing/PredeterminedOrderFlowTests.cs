66c83f35c9b217243b61a15abf604886a8b2f5af
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-01-25 08:52:50 +1300

    Have PredeterminedOrderFlowTests store the references to the mock units in an array, before using those references in teams.
    This is so GetFlowTestUnit is only ever called once per unit, as it clones them, meaning equality wouldn't work as it should.

--- Choose Your Own Adventure Game/Assets/Scripts/Unit Testing/PredeterminedOrderFlowTests.cs
+++ Choose Your Own Adventure Game/Assets/Scripts/Unit Testing/PredeterminedOrderFlowTests.cs
@@ -5,34 +5,54 @@ using System.Collections;
 using NSubstitute;
 using System.Collections.Generic;
 using UnityEditor;
+using System;
+using UnityScript.Lang;
 
 public class PredeterminedOrderFlowTests : MonoBehaviour {
 
-    private LinkedList<LinkedList<Unit>> testUnits = new LinkedList<LinkedList<Unit>>();
+    private Unit[] testUnits;
+    
+
+    private LinkedList<LinkedList<Unit>> testTeams = new LinkedList<LinkedList<Unit>>();
 
     [OneTimeSetUp]
     public void PrepareForTests()
     {
 
+        //Load in test units.
+        testUnits = new Unit[] {
+            CombatFlowTests.GetFlowTestUnit("Flow Test Mock Unit 1"),
+            CombatFlowTests.GetFlowTestUnit("Flow Test Mock Unit 2"),
+            CombatFlowTests.GetFlowTestUnit("Flow Test Mock Unit 3"),
+            CombatFlowTests.GetFlowTestUnit("Flow Test Mock Unit 4"),
+            CombatFlowTests.GetFlowTestUnit("Flow Test Mock Unit 5"),
+            CombatFlowTests.GetFlowTestUnit("Flow Test Mock Unit 6"),
+            CombatFlowTests.GetFlowTestUnit("Flow Test Mock Unit 7"),
+            CombatFlowTests.GetFlowTestUnit("Flow Test Mock Unit 8"),
+            CombatFlowTests.GetFlowTestUnit("Flow Test Mock Unit 9"),
+            CombatFlowTests.GetFlowTestUnit("Flow Test Mock Unit 10")
+        };
+
+
         //The default mock team setup.
         List<List<Unit>> mockTeams = new List<List<Unit>>( new List<Unit>[] {
-            new List<Unit>( new Unit[]{
-                CombatFlowTests.GetFlowTestUnit("Flow Test Mock Unit 1"),
-                CombatFlowTests.GetFlowTestUnit("Flow Test Mock Unit 2"),
-                CombatFlowTests.GetFlowTestUnit("Flow Test Mock Unit 3")
+            new List<Unit>( new Unit[] {
+                testUnits[0],
+                testUnits[1],
+                testUnits[2]
             }),
 
             new List<Unit>( new Unit[] {
-               CombatFlowTests.GetFlowTestUnit("Flow Test Mock Unit 4"),
-               CombatFlowTests.GetFlowTestUnit("Flow Test Mock Unit 5"),
-               CombatFlowTests.GetFlowTestUnit("Flow Test Mock Unit 6")
+                testUnits[3],
+                testUnits[4],
+                testUnits[5],
             }),
 
-            new List<Unit>( new Unit[]{
-                CombatFlowTests.GetFlowTestUnit("Flow Test Mock Unit 7"),
-                CombatFlowTests.GetFlowTestUnit("Flow Test Mock Unit 8"),
-                CombatFlowTests.GetFlowTestUnit("Flow Test Mock Unit 9"),
-                CombatFlowTests.GetFlowTestUnit("Flow Test Mock Unit 10")
+            new List<Unit>( new Unit[] {
+                testUnits[6],
+                testUnits[7],
+                testUnits[8],
+                testUnits[9]
             }),
 
         });
@@ -47,12 +67,9 @@ public class PredeterminedOrderFlowTests : MonoBehaviour {
             for (int j = 0; j < mockTeams[i].Count; j++)
                 currentTeam.AddLast(mockTeams[i][j]);
 
-            testUnits.AddLast(currentTeam);
+            testTeams.AddLast(currentTeam);
 
         }
-
-        Debug.Log("testUnits.Count: " + testUnits.Count);
-
 
     }
 
@@ -63,7 +80,7 @@ public class PredeterminedOrderFlowTests : MonoBehaviour {
     public void TeamNumberIsIncrementedAndLooped()
     {
 
-        PredeterminedOrderFlow flow = new PredeterminedOrderFlow(testUnits);
+        PredeterminedOrderFlow flow = new PredeterminedOrderFlow(testTeams);
 
         for(int i = 0; i < 100; i++)
         {
@@ -72,7 +89,7 @@ public class PredeterminedOrderFlowTests : MonoBehaviour {
 
             flow.TakeTurn(new List<Unit>(flow.UnitsAvaliableForTurn)[0]);
 
-            if (teamBefore == testUnits.Count - 1)
+            if (teamBefore == testTeams.Count - 1)
                 Assert.IsTrue(flow.CurrentTeam == 0, "The team number should loop back to 0 when the max is reached.");
             else
                 Assert.IsTrue(flow.CurrentTeam == teamBefore + 1,
@@ -82,16 +99,45 @@ public class PredeterminedOrderFlowTests : MonoBehaviour {
 
     }
 
-    //[Test]
+    /**
+     * Ensures every turn only has one unit avaliable.
+     */ 
+    [Test]
+    public void OnlyOneUnitIsEverAvaliable()
+    {
+
+        PredeterminedOrderFlow flow = new PredeterminedOrderFlow(testTeams);
+
+        for (int i = 0; i < 100; i++)
+        {
+
+            int teamBefore = flow.CurrentTeam;
+
+            Assert.AreEqual(1, flow.UnitsAvaliableForTurn.Count);
+
+            flow.TakeTurn(new List<Unit>(flow.UnitsAvaliableForTurn)[0]);
+
+        }
+
+    }
+
+    [Test]
     public void CorrectUnitsAreSelectedAtTheRightTime()
     {
 
-        CombatFlow flow = new PredeterminedOrderFlow(testUnits);
+        CombatFlow flow = new PredeterminedOrderFlow(testTeams);
 
-        foreach(LinkedList<Unit> team in testUnits)
+        int[] mockTestUnitOrder = new int[] { 1, 4, 7, 2, 5, 8, 3, 6, 9, 1, 4, 10 };
+
+        foreach(int unitNumber in mockTestUnitOrder)
         {
 
+            Unit expectedUnit = testUnits[unitNumber - 1];
+            Assert.IsTrue(flow.UnitsAvaliableForTurn.Contains(expectedUnit),
+                          expectedUnit + " should be avaliable. /n" +
+                          "Avaliable Units: " + String.Format(TestingUtil.PrintsItemsOf(flow.UnitsAvaliableForTurn)));
 
+            flow.TakeTurn(new List<Unit>(flow.UnitsAvaliableForTurn)[0]);
 
         }
 
@@ -102,7 +148,7 @@ public class PredeterminedOrderFlowTests : MonoBehaviour {
     public void ResetHealthOfAllUnits()
     {
 
-        foreach (LinkedList<Unit> team in testUnits)
+        foreach (LinkedList<Unit> team in testTeams)
             foreach (Unit unit in team) unit.health = unit.maxHealth;
 
     }
