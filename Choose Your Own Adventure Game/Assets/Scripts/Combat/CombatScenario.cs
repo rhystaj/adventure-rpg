0f8af69682efb9e46f3fa613964497d04a1cc3d8
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-01-28 08:39:17 +1300

    Change CombatBoard to CombatScenario, and have it focused on the back end.

---
+++ Choose Your Own Adventure Game/Assets/Scripts/Combat/CombatScenario.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * The back-end of combat.
+ */
+public class CombatScenario
+{
+
+    private Unit[] board; //The grid of units.
+    private CombatEncounter encounter; //The encounter taking place.
+
+
+
+}
