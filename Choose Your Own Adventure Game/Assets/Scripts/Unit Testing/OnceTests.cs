5f66cbb1409e204b0351b07d48fa3765be21f353
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-01-30 14:32:14 +1300

    Create test class for Once.

---
+++ Choose Your Own Adventure Game/Assets/Scripts/Unit Testing/OnceTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnceTests {
+
+	[Test]
+    public void ValueInOnceCanBeAssignedOnce()
+    {
+
+        Once<string> testOnce = new Once<string>();
+        testOnce.Value = "test";
+
+    }
+
+    [Test]
+    public void ValueInOnceCanNotBeReassigned()
+    {
+
+        Once<string> testOnce = new Once<string>();
+        testOnce.Value = "test";
+
+        try
+        {
+            testOnce.Value = "test2";
+            Assert.Fail();
+        }
+        catch(Once<string>.ValueAlreadyAssignedException e) { }
+
+    }
+
+}
