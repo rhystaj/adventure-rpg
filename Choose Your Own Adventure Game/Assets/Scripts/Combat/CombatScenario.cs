d207caf8d21064e5a5169a3b81e9aaca680418fb
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-01-30 14:35:36 +1300

    Add assertions to CombatScenario constructor and make fields instances on Once.

--- Choose Your Own Adventure Game/Assets/Scripts/Combat/CombatScenario.cs
+++ Choose Your Own Adventure Game/Assets/Scripts/Combat/CombatScenario.cs
@@ -9,8 +9,8 @@ using UnityEngine.Assertions;
 public class CombatScenario<F> where F : CombatFlow
 {
 
-    private Unit[,] board; //The grid of units.
-    private CombatFlow flow; //The struture of the progression of the combat.
+    private Once<Unit[,]> board; //The grid of units.
+    private Once<CombatFlow> flow; //The struture of the progression of the combat.
 
     public CombatScenario(Unit[,] playerUnits, CombatEncounter encounter, CombatFlow.Adaptor flowAdaptor)
     {
@@ -19,27 +19,29 @@ public class CombatScenario<F> where F : CombatFlow
         Assert.IsTrue(playerUnits.GetLength(0) == encounter.rows && playerUnits.GetLength(1) == encounter.columnsPerSide,
                       "Precondition Fail: The player configuration should have the same dimensions and the enemy configuration.");
         Assert.IsNotNull(encounter, "Precondition Fail: The argument 'encounter' should not be null.");
+        Assert.IsNotNull(flowAdaptor, "Precondition Fail: The argument 'flowAdaptor' should not be null.");
 
 
         //Create a new board
-        board = new Unit[encounter.rows, encounter.columnsPerSide * 2]; //Double as columns per side is the number of units on ONE of the TWO sides.
+        board.Value = new Unit[encounter.rows, encounter.columnsPerSide * 2]; //Double as columns per side is the number of units on ONE of the TWO sides.
 
 
         //Copy the enemy units from the encounter to the enemy side.
-        for (int i = 0; i < board.GetLength(0); i++)
+        for (int i = 0; i < board.Value.GetLength(0); i++)
         {
-            for (int j = encounter.columnsPerSide; j < board.GetLength(1); j++)
+            for (int j = encounter.columnsPerSide; j < board.Value.GetLength(1); j++)
             {
                 Unit enemy = encounter.enemyConfiguration[(j - encounter.columnsPerSide) + (encounter.columnsPerSide * i)];
-                board[i, j] = Object.Instantiate(enemy) as Unit;
+                board.Value[i, j] = Object.Instantiate(enemy) as Unit;
             }
         }
 
-        flow = flowAdaptor.Convert(playerUnits, encounter);
+        flow.Value = flowAdaptor.Convert(playerUnits, encounter);
 
         //Postconditions
         Assert.IsNotNull(board, "Postcondition Fail: The field 'board' should not be null.");
-        Assert.IsTrue(board.Length == encounter.columnsPerSide * encounter.rows * 2);
+        Assert.IsTrue(board.Value.Length == encounter.columnsPerSide * encounter.rows * 2);
+        Assert.IsNotNull(flow, "Postcondition Fail: The field 'flowAdaptor' should not be null.");
 
     }
 
