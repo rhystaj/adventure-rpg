c5763a0bde131020ade552652bf4ae917e73f912
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-01-30 15:54:21 +1300

    Fill out CombatScenario
    Add UseInstrument method.
    Add getter that returns a copy of the board.
    Create WinTracker interface for a Stategy approach to win conditions.

--- Choose Your Own Adventure Game/Assets/Scripts/Combat/CombatScenario.cs
+++ Choose Your Own Adventure Game/Assets/Scripts/Combat/CombatScenario.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Assertions;
 
 /**
@@ -11,8 +9,28 @@ public class CombatScenario
 
     private Once<Unit[,]> board; //The grid of units.
     private Once<CombatFlow> flow; //The struture of the progression of the combat.
+    private Once<WinTracker> winTracker; //Tracks the progress of the game to determine the winner.
 
-    public CombatScenario(Unit[,] playerUnits, CombatEncounter encounter, CombatFlow.Adaptor flowAdaptor)
+    public delegate void TeamEvent(int team);
+    public TeamEvent OnTeamWin;
+
+    /**
+     * Return a copy of the board.
+     */
+    public Unit[,] Board {
+
+        get {
+
+            Unit[,] clone = new Unit[board.Value.GetLength(0), board.Value.GetLength(1)];
+            System.Array.Copy(board.Value, clone, clone.Length);
+
+            return clone;
+
+        }
+
+    }
+
+    public CombatScenario(Unit[,] playerUnits, CombatEncounter encounter, CombatFlow.Adaptor flowAdaptor, WinTracker winTracker)
     {
 
         //Preconditions
@@ -20,7 +38,7 @@ public class CombatScenario
                       "Precondition Fail: The player configuration should have the same dimensions and the enemy configuration.");
         Assert.IsNotNull(encounter, "Precondition Fail: The argument 'encounter' should not be null.");
         Assert.IsNotNull(flowAdaptor, "Precondition Fail: The argument 'flowAdaptor' should not be null.");
-
+        Assert.IsNotNull(winTracker, "Precondition Fail: The argument 'winTracker' should not be null.");
 
         //Create a new board
         board.Value = new Unit[encounter.rows, encounter.columnsPerSide * 2]; //Double as columns per side is the number of units on ONE of the TWO sides.
@@ -38,10 +56,58 @@ public class CombatScenario
 
         flow.Value = flowAdaptor.Convert(playerUnits, encounter);
 
+
         //Postconditions
         Assert.IsNotNull(board, "Postcondition Fail: The field 'board' should not be null.");
         Assert.IsTrue(board.Value.Length == encounter.columnsPerSide * encounter.rows * 2);
         Assert.IsNotNull(flow, "Postcondition Fail: The field 'flowAdaptor' should not be null.");
+        Assert.IsNotNull(winTracker, "Postcondition Fail: The field 'winTracker' should not be null.");
+
+    }
+
+    /**
+     * Try to register a turn in which a unit uses their instrument on another.
+     */
+    public bool UseInstrument(Unit subject, Unit target)
+    {
+
+        //Preconditions
+        Assert.IsNotNull(subject, "Precondition Fail: The argument 'subject' should not be null.");
+        Assert.IsNotNull(target, "Precondition Fail: The argument 'target' should not be null.");
+
+
+        bool result = false;
+
+        if (flow.Value.CanMoveDuringCurrentTurn(subject) && subject.UseInstrument(target))
+        {
+            flow.Value.TakeTurn(subject);
+            winTracker.Value.Update(board.Value);
+            result = true;
+        }
+
+        if (result)
+        {
+            int winner = winTracker.Value.DetermineWinner(board.Value);
+            if (winner != -1) OnTeamWin(winner);
+        }
+
+
+        return result;
+    }
+
+
+    public interface WinTracker
+    {
+
+        /**
+         * Updates the status of the tracker that will go towards determining the winner.
+         */ 
+        void Update(Unit[,] board);
+
+        /**
+         * Returns the winning team, or -1 if there is none.
+         */ 
+        int DetermineWinner(Unit[,] board);
 
     }
 
