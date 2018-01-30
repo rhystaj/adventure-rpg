018ed76f8b239676b74f668e67bb3ec18589419f
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-01-30 14:16:22 +1300

    Create Once util class.

---
+++ Choose Your Own Adventure Game/Assets/Scripts/Util/Once.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+/**
+ * A generic proxy class that does not allow its value to be reassigned once it is not null.
+ */
+public class Once<T> {
+
+    private T _value;
+    public T Value
+    {
+        get { return _value; }
+        set
+        {
+            if (_value == null) _value = value;
+            else throw new ValueAlreadyAssignedException();
+        }
+
+    }
+
+    public class ValueAlreadyAssignedException : Exception { }
+
+}
