b2ad5d2750f0c7fb2a0d70d1c94dfd63a5bb6271
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-02-03 15:04:19 +1300

    Create CombatBoard MonoBehaviour.
    Intended to be the GUI for the combat.

---
+++ Choose Your Own Adventure Game/Assets/Scripts/Combat/CombatBoard.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatBoard : MonoBehaviour {
+
+    private ICombatScenario combatScenario; //The combat scenario the board is displaying. 
+
+}
