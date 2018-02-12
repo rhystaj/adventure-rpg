d09fed5150cd2f074e2ee9d9ff4500dd28a11de4
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-02-12 13:37:18 +1300

    Create a basic initialisation script for testing and add board to test scene.

---
+++ Choose Your Own Adventure Game/Assets/Testing/Combat Test/CombatTestInit.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatTestInit : MonoBehaviour {
+
+    public CombatBoard board;
+    public CombatEncounter encounter;
+    public Unit playerUnit;
+
+    private void Start()
+    {
+
+        Unit.Instance[,] playerUnits = new Unit.Instance[,]
+        {
+            { new Unit.Instance(playerUnit), new Unit.Instance(playerUnit) },
+            { new Unit.Instance(playerUnit), new Unit.Instance(playerUnit) }
+        };
+
+        CombatScenario scenario = new CombatScenario(playerUnits, encounter,
+                                                     new CombatFlow.DirectAdaptor(new SingleEmptyTurnMockCombatFlow(0, null)),
+                                                     new MockWinTracker(0, 5));
+
+        board.DisplayScenario(scenario);
+
+    }
+
+}
