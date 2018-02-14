97ffa4626e40b8fce7eb0c33d75755b5ba24a743
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-02-14 15:21:33 +1300

    Remove CombatAnimator as a subclass and fill in DetermineMove and SelectUnit methods.

--- Choose Your Own Adventure Game/Assets/Scripts/Combat/CombatBoard.cs
+++ Choose Your Own Adventure Game/Assets/Scripts/Combat/CombatBoard.cs
@@ -12,20 +12,19 @@ public class CombatBoard : MonoBehaviour, IController {
     [SerializeField] Vector2 distancesBetweenUnits;
     [SerializeField] float centreGapWidth;
 
-    private CombatScenario currentScenario; //The scenario being displayed on the board.
-    private CombatAnimator animator;
-
     private UnitVessel subject; //The unit taking the turn.
     private UnitVessel target; //The unit the turn's action is being performed on.
 
     private bool locked; //When true, the board can not be interacted with.
 
-    public void DisplayScenario(ICombatScenario scenario)
+    public UnitVessel[] vessels { get { return transform.GetComponentsInChildren<UnitVessel>(); } }
+
+    public void Display(Unit.IInstance[,] board)
     {
 
         //Preconditions
-        Assert.IsNotNull(scenario, "Precondition Fail: The argument 'combatScenario' should not be null.");
-        Assert.IsFalse(scenario.Board.Length <= 0, "Precondition Fail: The board of the given should contain at least one unit.");
+        Assert.IsNotNull(board, "Precondition Fail: The argument 'combatScenario' should not be null.");
+        Assert.IsFalse(board.Length <= 0, "Precondition Fail: The board of the given should contain at least one unit.");
 
 
         //Destroy all current children to make way for the new scenario.
@@ -36,7 +35,7 @@ public class CombatBoard : MonoBehaviour, IController {
 
 
         //Use the vessel base to create UI representations of the unit in combat board.
-        Unit.IInstance[,] board = scenario.Board;
+
         for(int row = 0; row < board.GetLength(0); row++)
         {
             for(int column = 0; column < board.GetLength(1); column++)
@@ -80,20 +79,32 @@ public class CombatBoard : MonoBehaviour, IController {
 
         if (!locked)
         {
-            Debug.Log(vessel.name + "selected");
+
+            if (subject == null) subject = vessel;
+            else if (target == null) target = vessel;
+
         }
 
     }
 
     public ICombatAction DetermineMove(Unit.IInstance[,] board, HashSet<Unit.IInstance> avaliableUnits)
     {
-        throw new NotImplementedException();
+        if (subject != null && target != null) {
+            Debug.Log("Moving");
+
+            ICombatAction action = new InstrumentUse(subject.unit, target.unit); ;
+            subject = null;
+            target = null;
+
+            return action;
+        }
+        else return null;
     }
 
     /**
      * Represents a single interactable unit on the board.
-     */ 
-    private class UnitVessel : MonoBehaviour
+     */
+    public class UnitVessel : MonoBehaviour
     {
         public CombatBoard parent;
 
@@ -103,8 +114,9 @@ public class CombatBoard : MonoBehaviour, IController {
         private Unit.IInstance _unit;
         public Unit.IInstance unit
         {
-            set {
-                
+            set
+            {
+
                 //Preconditions
                 Assert.IsNotNull(parent, "The field 'parent' should not be null.");
                 Assert.IsNotNull(value, "The given value should not be null.");
@@ -145,41 +157,6 @@ public class CombatBoard : MonoBehaviour, IController {
         private void OnMouseDown()
         {
             parent.SelectUnit(this);
-        }
-
-    }
-
-    private class CombatAnimator : ICombatAnimator
-    {
-
-        private Dictionary<Unit.IInstance, UnitVessel> vessels = new Dictionary<Unit.IInstance, UnitVessel>(); //The instances of units mapped to the vessels that house them.
-
-        public CombatAnimator(UnitVessel[] vessels)
-        {
-
-            //Preconditions
-            Assert.IsNotNull(vessels, "The argument 'vessels' should not be null.");
-
-
-            foreach (UnitVessel vessel in vessels)
-                this.vessels.Add(vessel.unit, vessel);
-
-
-        }
-
-
-        public IEnumerator PoseUnit(Unit.IInstance unit, Unit.State poseState)
-        {
-
-            //Preconditions
-            Assert.IsNotNull(unit, "Precondition Fail: The argument 'unit' should not be null.");
-            Assert.IsTrue(vessels.ContainsKey(unit),
-                          "Precondition Fail: The given unit should be a key in state.");
-
-
-            vessels[unit].SetPose(poseState);
-            yield return null;
-
         }
 
     }
